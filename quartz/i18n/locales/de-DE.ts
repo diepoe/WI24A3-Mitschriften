bc9505a777775dd453e26b09f1ca33d6bf571e67
diepoe <poedietrich@outlook.de>
2024-12-03 10:43:14 +0100

    adjust localisation

--- quartz/i18n/locales/de-DE.ts
+++ quartz/i18n/locales/de-DE.ts
@@ -54,7 +54,7 @@ export default {
       title: "Inhaltsverzeichnis",
     },
     contentMeta: {
-      readingTime: ({ minutes }) => `${minutes} min read`,
+      readingTime: ({ minutes }) => `${minutes} min Lesezeit`,
     },
   },
   pages: {
@@ -65,7 +65,7 @@ export default {
     error: {
       title: "Nicht gefunden",
       notFound: "Diese Seite ist entweder nicht öffentlich oder existiert nicht.",
-      home: "Return to Homepage",
+      home: "Zurück zur Startseite",
     },
     folderContent: {
       folder: "Ordner",
